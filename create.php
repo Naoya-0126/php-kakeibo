8d92a20d64a6b964983032488523639f92f1c838
Naoya-0126 <115777716+Naoya-0126@users.noreply.github.com>
2022-10-19 15:42:55 +0800

    画面で入力された値を取得

--- create.php
+++ create.php
@@ -1,4 +1,18 @@
 <?php
+  //処理の流れ
+  //⒈画面で入力された値を取得
+  //2.PHPからMySQLへ接続
+  //3.SQL文を作成して、画面で入力された値をrecordsテーブルに追加
+  //4.index.phpに画面遷移する
+
+  
+  //⒈画面で入力された値を取得
+  $date = $_POST['date'];
+  $title = $_POST['title'];
+  $amount = $_POST['amount'];
+  $type = $_POST['type'];
+
+  
 
 
 ?>
