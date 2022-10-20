9b59ac9b8c175edefbdcc21c80ee63b989e7833e
Naoya-0126 <115777716+Naoya-0126@users.noreply.github.com>
2022-10-20 13:53:10 +0800

    create.phpを更新しました

--- create.php
+++ create.php
@@ -1,6 +1,6 @@
 <?php
 
-//dbconnect.phpを読み込む→DBに接続
+//2.dbconnect.phpを読み込む→DBに接続
 include_once('./dbconnect.php');
 
   //処理の流れ
@@ -15,6 +15,29 @@ include_once('./dbconnect.php');
   $title = $_POST['title'];
   $amount = $_POST['amount'];
   $type = $_POST['type'];
+
+
+  //SQL文を作成
+  $sql = "INSERT INTO records(title, type, amount, date, created_at, updated_at) VALUES(:title, :type, :amount, :date, now(), now())";
+
+  
+  //作成したSQLを実行できるように準備
+  $stmt = $pdo->prepare($sql);
+
+  //値の設定、何を保存するのかを設定  bind = 結びつける、 パラメーター = 外部から投入されたデータ
+  $stmt->bindParam(':title', $title, PDO::PARAM_STR);
+  $stmt->bindParam(':type', $type, PDO::PARAM_INT);
+  $stmt->bindParam(':amount', $amount, PDO::PARAM_INT);
+  $stmt->bindParam(':date', $date, PDO::PARAM_STR);
+
+  //SQLを実行
+  $stmt->execute();
+
+
+  header('Location: ./index.php');
+        exit;
+
+
 
   
 
